c8ac0611340da303574467099954f12ea24bf9f9
MalekAjil <aboanasajil@gmail.com>
2024-01-29 19:32:42 -0800

    task0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -2,6 +2,6 @@
 # Ruby script that accepts one argument and pass it to a regular expression matching method
 arg = ARGV[0]
 regex = /School/
-  if rgex.match(arg)
+  if regex.match(arg)
     puts "#{regex}"
 end

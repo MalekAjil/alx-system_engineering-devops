6d53fe09d0acc2199fa5a9b90f766bb7dc079af2
MalekAjil <aboanasajil@gmail.com>
2024-01-31 10:01:29 -0800

    task100

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,6 @@
 #!/usr/bin/env ruby
 # Ruby script that accepts one argument and pass it to a regular expression matching method
-puts ARGV[0].scan(/^\d{10}$/).join
+sender = ARGV[0].scan(/\[?from:(.*?)\]/).join
+reciever = ARGV[0].scan(/\[to:(.+?)\]/).join
+flags = ARGV[0].scan(/\[flags:(.+?)\]/).join
+puts "#{sender},#{reciever},#{flags}"

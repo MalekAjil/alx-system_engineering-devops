cf977d0f35c34ec84bd405d42c97e9636f14ef0e
MalekAjil <aboanasajil@gmail.com>
2024-01-30 18:48:31 -0800

    task9

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,7 +1,3 @@
 #!/usr/bin/env ruby
 # Ruby script that accepts one argument and pass it to a regular expression matching method
-arg = ARGV[0]
-regex = /School/
-  if regex.match(arg)
-    puts "#{regex}"
-end
+puts ARGV[0].scan(/School/).join

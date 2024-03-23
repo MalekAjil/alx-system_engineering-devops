b04f27cd403a4bfb432fa1225a289edfeb4b5d50
MalekAjil <aboanasajil@gmail.com>
2024-03-23 14:31:35 -0700

    task4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+class ssh_config {
+  file { '/etc/ssh/ssh_config':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('ssh/ssh_config.erb'),
+  }
+}

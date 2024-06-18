0987d88c44cede6b4edf53875f3206ce19ba9eb3
MalekAjil <aboanasajil@gmail.com>
2024-06-18 15:48:44 -0700

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 """
-Subscrips
+0-subs
 """
-import sys
+import requests
 
 
 def number_of_subscribers(subreddit):
+    """returns the number of subsecriptions"""
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    hdrs = {'User-Agent': 'custom user-agent'}
+    try:
+        res = requests.get(url, headers=hdrs, allow_redirections=False)
+        if response.status_code == 200:
+            return response.json()['data']['subscribers']
+        else:
+            return 0
+    except Exception as ex:
+        print(f'Error :: {ex}')
     return 0

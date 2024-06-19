b3c35bfe6f5c1fcbeaad1a5a17e5a594fd346373
MalekAjil <aboanasajil@gmail.com>
2024-06-18 18:01:25 -0700

    task1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,12 +6,13 @@ import requests
 
 
 def top_ten(subreddit):
-    """top_ten: prints the titles of the first 10 hot posts listed for a given subreddit."""
+    """top_ten: prints the titles of the first 10 hot posts
+    listed for a given subreddit."""
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     hdrs = {'User-Agent': 'python:top_ten:v1.0 (by/u/MalekAjil)'}
     res = requests.get(url, headers=hdrs, allow_redirects=False)
     if res.status_code != 200:
-        print(None)
+        print(0)
         return
     data = res.json()
     for post in darta['data']['children']:

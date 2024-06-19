b80ca525fea9833bec112bc035fb3248492eccae
MalekAjil <aboanasajil@gmail.com>
2024-06-19 14:59:00 -0700

    task0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,2 +1,11 @@
 #Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet (instead of using Bash as you were previously doing).
+class apache_config {
+  file_line { 'apache_server_name':
+    ensure => present,
+    path   => '/etc/apache2/apache2.conf',
+    line   => 'ServerName 100.25.46.143',
+    match  => '^#?ServerName',
+  }
+}
 
+include apache_config

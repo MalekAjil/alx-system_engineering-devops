62e615fbfa100e4509e678ccc258ac2f93b62c5d
MalekAjil <aboanasajil@gmail.com>
2024-06-18 17:56:49 -0700

    task1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 def number_of_subscribers(subreddit):
     """returns the number of subsecriptions"""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    hdrs = {'User-Agent': 'programming'}
+    hdrs = {'User-Agent': 'python:subs:v1.0'}
     try:
         res = requests.get(url, headers=hdrs, allow_redirects=False)
         if res.status_code == 200:
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,4 +6,13 @@ import requests
 
 
 def top_ten(subreddit):
-
+    """top_ten: prints the titles of the first 10 hot posts listed for a given subreddit."""
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    hdrs = {'User-Agent': 'python:top_ten:v1.0 (by/u/MalekAjil)'}
+    res = requests.get(url, headers=hdrs, allow_redirects=False)
+    if res.status_code != 200:
+        print(None)
+        return
+    data = res.json()
+    for post in darta['data']['children']:
+        print(post['data']['tit;e'])

8d674a97ca295634b89400f596ff0a2df243817d
MalekAjil <aboanasajil@gmail.com>
2024-07-29 16:33:16 -0700

    task1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+1-export_to_CSV Module
+a Python script that, using this REST API, for a given employee ID,
+exports data to csv file
+"""
+import csv
+import requests
+import sys
+
+
+def get_todo_list(empid):
+    """for the TODO list"""
+    api_url = "https://jsonplaceholder.typicode.com"
+
+    try:
+        res = requests.get(api_url + f"/users/{empid}")
+        resdata = res.json()
+
+        empname = resdata.get("name")
+
+        res2 = requests.get(api_url + f"/todos?userId={empid}")
+        resdata2 = res2.json()
+        with open(f"{empid}.csv", 'w', encoding='UTF8') as f:
+            writer = csv.writer(f)
+            for task in resdata2:
+                data = [empid, empname, task['completed'], task['title']]
+                writer.writerow(data)
+
+    except requests.RequestException as e:
+        print(f"Error fetching data: {e}")
+
+
+if __name__ == "__main__":
+    get_todo_list(sys.argv[1])

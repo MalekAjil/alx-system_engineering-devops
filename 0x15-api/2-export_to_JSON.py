fb779f86c818da04711722f2a0e2f34c23260031
MalekAjil <aboanasajil@gmail.com>
2024-07-29 17:26:21 -0700

    task2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+2-export_to_JSON Module
+a Python script that, using this REST API, for a given employee ID,
+exports data to json file
+"""
+import json
+import requests
+import sys
+
+
+def get_todo_list(empid):
+    """for the TODO list"""
+    api_url = "https://jsonplaceholder.typicode.com"
+
+    try:
+        res = requests.get(api_url + f"/users/{empid}")
+        resdata = res.json()
+
+        username = resdata.get("username")
+
+        res2 = requests.get(api_url + f"/todos?userId={empid}")
+        resdata2 = res2.json()
+        with open(f"{empid}.json", 'w', encoding='UTF8', newline='') as f:
+            data = [empid, []]
+            for task in resdata2:
+                tsk = {"task": task['title'],
+                       "completed": task['completed'],
+                       "username": username}
+                data[1].append(tsk)
+            json.dump(data, f)
+
+    except requests.RequestException as e:
+        print(f"Error fetching data: {e}")
+
+
+if __name__ == "__main__":
+    get_todo_list(sys.argv[1])

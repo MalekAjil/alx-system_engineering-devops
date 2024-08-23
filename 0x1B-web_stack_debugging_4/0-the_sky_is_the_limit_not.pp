6065c092782850d5c5a705d229321a61b6e252d3
MalekAjil <aboanasajil@gmail.com>
2024-08-22 19:12:37 -0700

    task0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,1 +1,12 @@
 #Sky is the limit, let's bring that limit higher
+#increase the limit of the default file
+exec { 'fix--for-nginx':
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
+}
+
+#restart nginx
+exec {'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
+}
